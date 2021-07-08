e7bf33f852ea7968b898f2fb55988151df1248db
15675336860 <1687699424@qq.com>
2021-07-08 15:35:22 +0800

    门诊病人信息

---
+++ src/main/java/com/example/model/pojos/Medicalcard.java
@@ -0,0 +1,19 @@
+package com.example.model.pojos;
+
+import java.io.Serializable;
+import lombok.Data;
+
+@Data
+public class Medicalcard implements Serializable {
+
+  private Integer mediNo;
+  private String mediCard;
+  private String mediPassword;
+  private Integer mediBalance;
+  private String mediIdcart;
+  private String mediPatientNo;
+  private java.sql.Timestamp mediNewdate;
+  private String mediState;
+
+  private static final long serialVersionUID = 1L;
+}
---
+++ src/main/java/com/example/model/pojos/Patient.java
@@ -0,0 +1,39 @@
+package com.example.model.pojos;
+
+import java.io.Serializable;
+import java.sql.Timestamp;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+
+@Data
+public class Patient implements Serializable {
+
+  private Integer patientNo;
+  private String patientName;
+  private String patientSex;
+  private String patientPhone;
+  private String patientIdcart;
+  private String patientAge;
+  private Timestamp patientBirthdate;
+  private String patientAddress;
+  private Timestamp patientDate;
+
+  public Patient(Integer patientNo, String patientName, String patientSex, String patientPhone, String patientIdcart, String patientAge, String patientAddress, Timestamp patientBirthdate, Timestamp patientDate) {
+    this.patientNo = patientNo;
+    this.patientName = patientName;
+    this.patientSex = patientSex;
+    this.patientPhone = patientPhone;
+    this.patientIdcart = patientIdcart;
+    this.patientAge = patientAge;
+    this.patientBirthdate = patientBirthdate;
+    this.patientAddress = patientAddress;
+    this.patientDate = patientDate;
+  }
+
+  public Patient() {
+
+  }
+
+  private static final long serialVersionUID = 1L;
+}
