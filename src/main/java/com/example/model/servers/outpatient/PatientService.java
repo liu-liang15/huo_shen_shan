142d549a4d9c3a643cff5316721358b105fd2c1a
15675336860 <1687699424@qq.com>
2021-07-08 15:26:44 +0800

    门诊病人信息

---
+++ src/main/java/com/example/model/servers/outpatient/PatientService.java
@@ -0,0 +1,28 @@
+package com.example.model.servers.outpatient;
+
+import com.example.model.dao.outpatient.PatientDao;
+import com.example.model.pojos.Patient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class PatientService {
+    @Autowired
+    PatientDao patientDao;
+
+    public List<Patient> findAll(String param){
+        return patientDao.findAllPatient(param);
+    }
+
+    public void insertPatient(Patient patient){
+        patientDao.insertPatient(patient);
+    }
+
+    public void updatePatient(Patient patient){
+        patientDao.updatePatient(patient);
+    }
+}
