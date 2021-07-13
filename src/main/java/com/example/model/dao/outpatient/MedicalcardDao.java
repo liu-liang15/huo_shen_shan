a2dee5d922944eca96ce01fec25f01ff5f47dfb0
17673978098 <2457342294@qq.com>
2021-07-13 16:45:15 +0800

    床位新增2.0

--- src/main/java/com/example/model/dao/outpatient/MedicalcardDao.java
+++ src/main/java/com/example/model/dao/outpatient/MedicalcardDao.java
@@ -1,5 +1,6 @@
 package com.example.model.dao.outpatient;
 
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.example.model.pojos.outpatient.Medicalcard;
 import org.apache.ibatis.annotations.Mapper;
 
